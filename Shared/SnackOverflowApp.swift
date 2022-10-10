0a8c03d258b9166fc3b8848c26ef065584513871
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2022-10-10 11:30:05 +0200

    Initial Commit

---
+++ Shared/SnackOverflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackOverflowApp.swift
+//  Shared
+//
+//  Created by Ali Riza Reisoglu on 10.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SnackOverflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
